d7f863c0878d21a79cc643f029fd612ffc8e9ab2
intro2cs-biu <intro2cs.biu@gmail.com>
2026-09-06 15:58:59 +0300

    Add files via upload

--- ex0-arduino.c
+++ ex0-arduino.c
@@ -1,9 +1,13 @@
-#include "arduino.h"
-#include "lcd.h"
+#include "api/arduino.h"
+#include "api/lcd.h"
+#include "serial.h"
+#include <stdio.h>
 
 int main(){
     pinMode(2, OUTPUT);
     lcdBegin(16, 2);
+    serialBegin(9600);
+    printf("Hello, World!\n");
     lcdPrintf("Hello, World!");
     digitalWrite(2, HIGH);
 }
